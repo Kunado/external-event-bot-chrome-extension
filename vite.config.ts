d1267c8e3ae5eb27a8b80e2c4badf327f597d966
Naoki Saruta <katsusand1@gmail.com>
2024-07-11 23:19:29 +0900

    Add manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import { crx } from "@crxjs/vite-plugin"
+import manifest from "./manifest.json"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [crx({ manifest }), react()],
 })
